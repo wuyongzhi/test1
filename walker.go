d8e33051f492f4e889b19a080a081f462debb0aa
yongzhi.wu <srdrm@icloud.com>
2016-06-11 18:32:57 +0800

    增加命令行参数处理

--- walker.go
+++ walker.go
@@ -2,23 +2,81 @@ package main
 
 import (
 	"crypto/sha1"
+	"flag"
 	"fmt"
 	"hash"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"sync"
 	"sync/atomic"
 )
 
 func printHelp() {
+	helperText := `功能:
+	遍历文件夹并计算其中文件的 SHA1 哈希值, 将结果输出到指定文件中
 
+用法:
+	遍历当前目录:
+		hashwalker
+
+	遍历指定目录:
+		hashwalker 目录名
+
+	遍历指定目录,并排除特定文件(支持通配符 *, ?):
+		hashwalker 目录名 --exclude *.txt [模式2,模式3...]
+
+	打印此帮助信息
+
+		hashwalker --help | -? | ?
+
+`
+
+	//log.Println("遍历文件夹并计算其中文件的 SHA1 哈希值, 将结果输出到指定文件中")
+	//log.Println()
+	//log.Println("\t用法: ")
+	//log.Println()
+	//
+	//log.Println("遍历当前目录")
+	//log.Println("\t\thashwalker")
+	//log.Println()
+	//
+	//log.Println("遍历指定目录")
+	//log.Println("\t\thashwalker 目录名 ")
+	//
+	//
+	//log.Println("\t\thashwalker 目录名 --exclude 排除模式1 [排除模式2...] ")
+
+	fmt.Print(helperText)
 }
 
-func parseCommandLine() {
+func parseCommandLine() *TaskDef {
 
+	excludesValue := flag.String("excludes", "", "忽略指定的文件,多个通配符之间用空格分开,并加双引号. 如: -excludes=\"*.jpg *.gif *.png\"")
+	rootValue := flag.String("root", "", "指定根目录,如果不指定,默认使用当前目录")
+	output := flag.String("output", "", "指定输出文件名,如果不指定,默认使用 hashwalker_output.txt")
+	r := regexp.MustCompile("\\s")
+	excludes := r.Split(*excludesValue, -1)
+
+	flag.Parse()
+
+	def := TaskDef{
+		Excludes: excludes,
+		Root:     *rootValue,
+		Output:   *output,
+	}
+
+	if def.Root == "" {
+		def.Root, _ = os.Getwd()
+	}
+	if def.Output == "" {
+		def.Output = "hashwalker_output.txt"
+	}
+
+	return &def
 }
 
 type FileHandler func(filePath string)
@@ -129,7 +187,8 @@ func do(def *TaskDef) {
 		if err != nil || info.IsDir() {
 			return nil
 		}
-		// TODO  排除忽略的文件
+
+		// TODO  处理跳过忽略的文件
 
 		item := WorkItem{
 			FilePath: path,
@@ -155,12 +214,11 @@ func main() {
 
 	// TODO 处理命令行参数
 
-	def := TaskDef{
-		Excludes: []string{"*.txt"},
-		Root:     "/Users/wuyongzhi/IdeaProjects/test1/exampleDir",
-		Output:   "walkerOutput.txt",
+	def := parseCommandLine()
+
+	if def == nil {
+		return
 	}
 
-	do(&def)
-
+	do(def)
 }
